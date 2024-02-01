1ce0099c4c630f8ad290d20180941f0d6c75b841
Danilo Mariano <ssadanilo@hotmail.com>
2024-02-01 17:27:09 -0300

    antecessorSucessor

--- antecessorSucessor.py
+++ antecessorSucessor.py
@@ -1,5 +1,5 @@
-num = int(input("digite um número inteiro: "))
+num = int(input('digite um número inteiro: '))
 ant = num - 1
 suc = num + 1
 
-print(f"Para o número {num}, seu antecessor é {ant}, e o sucessor é {suc}.")
+print(f'Para o número {num}, seu antecessor é {ant}, e o sucessor é {suc}.')
